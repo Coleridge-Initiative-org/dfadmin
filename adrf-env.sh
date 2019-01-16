ac751d6fe252506f25635ff1f3953c793cdc5480
Daniel Castellani <dancastellani@gmail.com>
2019-01-16 10:22:32 -0500

    Added coverage config and updated README with instructions for first time (no previous db backup).

--- adrf-env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export LDAP_BASE_DN='dc=adrf,dc=info'
-export LDAP_SERVER="ldap://auth.adrf.info"
-export LDAP_ADMIN_GROUP="cn=admin-tech,cn=groups"
-export LDAP_USER_SEARCH="ou=People"
-export LDAP_GROUP_SEARCH="ou=Groups"
