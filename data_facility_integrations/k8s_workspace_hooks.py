afda7867f952ca9e166727e6d375347c06e65bc5
Daniel Castellani <dancastellani@gmail.com>
2019-02-22 18:07:33 -0500

    Fixed error on k8s hooks and improved RDS-hooks

--- data_facility_integrations/k8s_workspace_hooks.py
+++ data_facility_integrations/k8s_workspace_hooks.py
@@ -20,13 +20,22 @@ DEFAULT_K8S_CONFIG = {
 @receiver(post_save, sender=Project)
 def prepare_default_config_workspace_k8s(sender, instance, **kwargs):
     K8S = ProjectTool.TOOL_CHOICES.Workspace_K8s
-    logger.debug('called project_saved - from "{0}" with params: {1}'.format(sender, kwargs))
+    logger.debug('called prepare_default_config_workspace_k8s - from "{0}" with params: {1}'.format(sender, kwargs))
     project = instance
     try:
         k8s_config = ProjectTool.objects.get(project=project, tool_name=K8S)
         if not k8s_config.system_info:
+            logger.debug('Initialized the default config for K8S')
             k8s_config.system_info = DEFAULT_K8S_CONFIG
+            k8s_config.save()
+        else:
+            logger.debug('Project %s already configured' % project)
+
     except ProjectTool.DoesNotExist:
         ProjectTool.objects.create(project=project, tool_name=K8S, system_info=DEFAULT_K8S_CONFIG)
         logger.info("Created new K8S config for project %s" % project.ldap_name)
-    logger.debug("Project Default Config - END")
+    except Exception as ex:
+        logger.error('Error on prepare_default_config_workspace_k8s for project %s' % project)
+        logger.exception(ex)
+
+    # logger.debug("prepare_default_config_workspace_k8s - END")
