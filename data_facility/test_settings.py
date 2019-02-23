0cfe54ebe2e199f39d8800553f964f1bdf4e613a
Daniel Castellani <dancastellani@gmail.com>
2019-02-22 19:22:21 -0500

    Fixed wrong reference to expected_outcomes

--- data_facility/test_settings.py
+++ data_facility/test_settings.py
@@ -1,7 +1,7 @@
 from settings import *
 
 print('USING TEST SETTINGS...')
-print('logging configuration is updated for tests: logs go to ')
+print('logging configuration is updated for tests: logs go to "FILE" only.')
 
 RDS_INTEGRATION = False
 WS_K8S_INTEGRATION = False
