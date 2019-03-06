b818cfedc52e912c8739e40cd88968c29cf388fb
Daniel Castellani <dancastellani@gmail.com>
2019-03-05 20:05:11 -0500

    Data Provider also working with slug instead of ID. Dataset reference still need refactoring

--- data_facility_admin/metadata_serializer.py
+++ data_facility_admin/metadata_serializer.py
@@ -23,18 +23,16 @@ def __get_classification_model(gmeta_classification):
     return None
 
 
-def __get_data_provider(metadata):
+def __get_data_provider(data_provider_name):
+    logger.debug('Data Provider: %s' % data_provider_name)
+
     try:
-        if DATA_PROVIDER_KEY in metadata:
-            data_provider_name = metadata[DATA_PROVIDER_KEY]
-            metadata.pop(DATA_PROVIDER_KEY, None)
-            dp = DataProvider.objects.get(name=data_provider_name)
-            return dp
-        else:
-            return None
+        return DataProvider.objects.get(name=data_provider_name)
     except DataProvider.DoesNotExist:
         logger.info('Created Data Provider: %s' % data_provider_name)
-        return DataProvider.objects.create(name=data_provider_name)
+        dp = DataProvider(name=data_provider_name)
+        dp.save()
+        return dp
 
 
 FieldMapping = namedtuple('FieldMapping', ['dataset', 'gmeta', 'mapping'])
@@ -44,7 +42,7 @@ DIRECT_FIELD_MAPPINGS = [
     FieldMapping('dataset_id', 'dataset_id', None),
     FieldMapping('version', 'dataset_version', None),
     FieldMapping('dataset_citation', 'dataset_citation', None),
-    FieldMapping('data_provider', 'data_provider', __get_data_provider),
+    # FieldMapping('data_provider', 'data_provider', __get_data_provider),
     # FieldMapping('keywords', 'keywords'),
 ]
 
@@ -75,11 +73,12 @@ def load(search_gmeta, detailed_gmeta=None, given_dataset=None):
         try:
             value = search_content[field_mapping.gmeta]
             #If there is a method to map, call it.
-            if field_mapping.mapping:
-                value = field_mapping.mapping(value)
+
             logger.debug('Dataset field "{0}" from gmeta field "{1}" = {2}'.format(field_mapping.dataset,
                                                                                    field_mapping.gmeta,
                                                                                    value))
+            if field_mapping.mapping:
+                value = field_mapping.mapping(value)
             # Set attribute on dataset and then remove the field from gmeta to avoid duplicates
             setattr(dataset, field_mapping.dataset, value)
             search_content.pop(field_mapping.gmeta, None)
@@ -89,7 +88,7 @@ def load(search_gmeta, detailed_gmeta=None, given_dataset=None):
 
     # Add other fields that need nested entities
     dataset.data_classification = __get_classification_model(search_content['data_classification'])
-    dataset.data_provider = __get_data_provider(search_content)
+    dataset.data_provider = __get_data_provider(search_content[DATA_PROVIDER_KEY])
 
     dataset.search_gmeta = search_content
     dataset.detailed_gmeta = detailed_gmeta
