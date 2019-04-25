926d16a42b8e89f69b21f0a3567ccb706bec4fb4
Daniel Castellani <dancastellani@gmail.com>
2019-04-25 10:40:05 -0400

    Removed partial test config from settings

--- data_facility/settings.py
+++ data_facility/settings.py
@@ -416,11 +416,6 @@ DATABASES = {
         'HOST': config('DATABASES_HOST'),
         'PORT': config('DATABASES_PORT', cast=int, default=5432),
     },
-    'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
 }
 
 # Internationalization
