7c0263e956cd3adf2c40d840f3c019ae0884ac36
Daniel Castellani <dancastellani@gmail.com>
2019-05-15 13:49:46 -0400

    update.sh is not expecting the service to be running anymore.

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Stop current service and kill processes if pending
-sudo supervisorctl stop dfadmin && \
+sudo supervisorctl stop dfadmin || \
 pkill '/usr/bin/uwsgi --ini /opt/dfadmin_config/deploy/adrf/uwsgi.ini' -f && \
 
 # Upgrade pip and install new requirements
