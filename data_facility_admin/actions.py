6fda2ca3f41e6ab0ed6db4f674a1eed047f18155
Daniel Castellani <dancastellani@gmail.com>
2019-06-13 11:51:06 -0400

    Reviewed user actions and added Reset OTP only and Password Only. (refs #25)

--- data_facility_admin/actions.py
+++ data_facility_admin/actions.py
@@ -6,7 +6,7 @@ from data_facility_admin.helpers import KeycloakHelper, EmailHelper
 def user_unlock(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     queryset.update(status=User.STATUS_UNLOCKED_BY_ADMIN)
-user_unlock.short_description = "Unlock selected users."
+user_unlock.short_description = "Unlock selected users"
 
 
 def user_disable(modeladmin, request, queryset):
@@ -24,13 +24,27 @@ user_activate.short_description = "Activate selected users (Status will be New)"
 def user_send_welcome_email(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     users = queryset.all()
-    KeycloakHelper().send_welcome_email(users)
-user_send_welcome_email.short_description = "Re-send the welcome email."
+    KeycloakHelper().send_welcome_email(users, reset_pwd=True, reset_otp=True)
+user_send_welcome_email.short_description = "Re-send the welcome email. (Reset Password + OTP)"
+
+
+def user_reset_pwd_only(modeladmin, request, queryset):
+    ''' Unlock selected users. '''
+    users = queryset.all()
+    KeycloakHelper().send_welcome_email(users, reset_pwd=True, reset_otp=False)
+user_reset_pwd_only.short_description = "Reset User Password (only)"
+
+
+def user_reset_otp_only(modeladmin, request, queryset):
+    ''' Unlock selected users. '''
+    users = queryset.all()
+    KeycloakHelper().send_welcome_email(users, reset_otp=True, reset_pwd=False)
+user_reset_otp_only.short_description = "Reset User OTP (only)"
 
 
 def user_send_rules_of_behavior(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     users = queryset.all()
     EmailHelper.send_updated_rules_of_behavior_email(users)
-user_send_welcome_email.short_description = "Send ADRF Rules of Behavior email."
+user_send_rules_of_behavior.short_description = "Send ADRF Rules of Behavior email"
 
