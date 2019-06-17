59f777d8e95b9a221da444c99513670b5202c350
Daniel Castellani <dancastellani@gmail.com>
2019-06-17 12:53:58 -0400

    Added sucess messages for users.

--- data_facility_admin/actions.py
+++ data_facility_admin/actions.py
@@ -9,18 +9,21 @@ from django.contrib import messages
 def user_unlock(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     queryset.update(status=User.STATUS_UNLOCKED_BY_ADMIN)
+    messages.success(request, "Success! Users unlocked.")
 user_unlock.short_description = "Unlock selected users"
 
 
 def user_disable(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     queryset.update(status=User.STATUS_DISABLED)
+    messages.success(request, "Success! Users disabled.")
 user_disable.short_description = "Disable selected users (Use instead of Delete)"
 
 
 def user_activate(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     queryset.filter(status=User.STATUS_PENDING_APPROVAL).update(status=User.STATUS_NEW)
+    messages.success(request, "Success! Users activated")
 user_activate.short_description = "Activate selected users (Status will be New)"
 
 
@@ -29,11 +32,11 @@ def user_send_welcome_email(modeladmin, request, queryset):
     users = queryset.all()
     try:
         KeycloakHelper().send_welcome_email(users, reset_pwd=True, reset_otp=settings.ADRF_MFA_ACTIVATED)
+        messages.success(request, "Success! Email sent to user.")
     except (ConnectionError, TooManyRedirects, HTTPError) as ex:
         messages.error(request, "Error updating password on Keycloak.")
     except Exception as ex:
         messages.error(request, "Error sending email.")
-
 user_send_welcome_email.short_description = "Re-send the welcome email. (Reset Password + OTP)"
 
 
@@ -42,11 +45,11 @@ def user_reset_pwd_only(modeladmin, request, queryset):
     users = queryset.all()
     try:
         KeycloakHelper().send_welcome_email(users, reset_pwd=True, reset_otp=False)
+        messages.success(request, "Success! Email sent to user.")
     except (ConnectionError, TooManyRedirects, HTTPError) as ex:
         messages.error(request, "Error updating password on Keycloak. The email was not sent.")
     except Exception as ex:
         messages.error(request, "Error sending email.")
-
 user_reset_pwd_only.short_description = "Reset User Password (only)"
 
 
@@ -55,6 +58,7 @@ def user_reset_otp_only(modeladmin, request, queryset):
     users = queryset.all()
     try:
         KeycloakHelper().send_welcome_email(users, reset_otp=True, reset_pwd=False)
+        messages.success(request, "Success! Email sent to user.")
     except (ConnectionError, TooManyRedirects, HTTPError) as ex:
         messages.error(request, "Error updating password on Keycloak.")
     except Exception as ex:
@@ -66,5 +70,6 @@ def user_send_rules_of_behavior(modeladmin, request, queryset):
     ''' Unlock selected users. '''
     users = queryset.all()
     EmailHelper.send_updated_rules_of_behavior_email(users)
+    messages.success(request, "Success! Email sent to user.")
 user_send_rules_of_behavior.short_description = "Send ADRF Rules of Behavior email"
 
