ee02d953c6098223fa655a80fce062d52bc95e03
Daniel Castellani <dancastellani@gmail.com>
2019-06-20 17:25:24 -0400

    Updated version

--- data_facility/settings.py
+++ data_facility/settings.py
@@ -21,7 +21,7 @@ import json
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 #DFAdmin Version
-VERSION = 'v2.3.1'
+VERSION = 'v2.3.2'
 
 ENV = config('ENV', default='PRODUCTION')
 ENVIRONMENT_COLORS = {'PRODUCTION': 'red',
