a3261db65dd0dde07080ed02e3166d65bb1f5489
Daniel Castellani <dancastellani@gmail.com>
2019-06-20 14:35:37 -0400

    Added tests for User-DFRole FILTER

--- data_facility_admin/test_querysets.py
+++ data_facility_admin/test_querysets.py
@@ -68,3 +68,40 @@ class ProjectQueryTests(TestCase):
         p = ProjectFactory.create(status=Project.STATUS_ACTIVE, end=YESTERDAY)
         results = Project.objects.filter(Project.FILTER_ACTIVE)
         self.assertEqual(len(results), 0)
+
+
+# pylint: disable=too-many-public-methods
+class UserDfRoleQueryTests(TestCase):
+    ''' Basic List and Add admin tests.'''
+
+    # @classmethod
+    # def setUpClass(cls):
+    #     super(ProjectQueryTests, cls).setUpClass()
+
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.role = DfRole.objects.create(name='My Test Role')
+
+    def test_filter_active_with_begin_past_returns_object(self):
+        user_role = UserDfRole.objects.create(user=self.user, role=self.role, begin=YESTERDAY)
+        results = UserDfRole.objects.filter(UserDfRole.FILTER_ACTIVE)
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], user_role)
+
+    def test_filter_active_with_begin_future_returns_nothing(self):
+        user_role = UserDfRole.objects.create(user=self.user, role=self.role, begin=TOMORROW)
+        results = UserDfRole.objects.filter(UserDfRole.FILTER_ACTIVE)
+        self.assertEqual(len(results), 0)
+
+    def test_filter_active_with_end_future_returns_object(self):
+        user_role = UserDfRole.objects.create(user=self.user, role=self.role, begin=YESTERDAY, end=TOMORROW)
+        results = UserDfRole.objects.filter(UserDfRole.FILTER_ACTIVE)
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0], user_role)
+
+    def test_filter_active_with_end_past_returns_nothing(self):
+        user_role = UserDfRole.objects.create(user=self.user, role=self.role, begin=YESTERDAY, end=YESTERDAY)
+        results = UserDfRole.objects.filter(UserDfRole.FILTER_ACTIVE)
+        self.assertEqual(len(results), 0)
+
+
